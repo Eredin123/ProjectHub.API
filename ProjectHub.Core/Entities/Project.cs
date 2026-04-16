fcd5c654f34e2f80c1f56a754b47ab0275667e3c
Altan Sarican <sarican@geomapping.de>
2026-04-16 20:08:31 +0200

    feat: add Project and ProjectTask entities

--- ProjectHub.Core/Entities/Project.cs
+++ ProjectHub.Core/Entities/Project.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ProjectHub.Core.Entities;
 
-namespace ProjectHub.Core.Entities
+public class Project
 {
-    internal class Project
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public ICollection<ProjectTask> Tasks { get; set; } = new List<ProjectTask>();
 }
--- ProjectHub.Core/Entities/ProjectTask.cs
+++ ProjectHub.Core/Entities/ProjectTask.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ProjectHub.Core.Entities;
 
-namespace ProjectHub.Core.Entities
+public class ProjectTask
 {
-    internal class ProjectTask
-    {
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool IsCompleted { get; set; } = false;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    public int ProjectId { get; set; }
+    public Project Project { get; set; } = null!;
 }
